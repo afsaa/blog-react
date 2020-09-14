2d4e0b33221d32a7843ac172b3f1968528edf479
Andrés Saam <andressaa.dev@gmail.com>
2020-09-14 14:53:16 -0500

    Eliminar usuario - Agregando la acción

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -4,34 +4,40 @@ import {
     LOGGED_IN_REQUEST,
     GET_USERS_REQUEST,
     GET_POSTS_REQUEST,
-    LOGOUT_REQUEST
+    LOGOUT_REQUEST,
+    DELETE_USER_REQUEST,
 } from "./actionTypes";
 
-export const loginRequest = payload => ({
+export const loginRequest = (payload) => ({
     type: LOGIN_REQUEST,
-    payload
+    payload,
 });
 
-export const registerRequest = payload => ({
+export const registerRequest = (payload) => ({
     type: REGISTER_REQUEST,
-    payload
+    payload,
 });
 
-export const loggedInRequest = payload => ({
+export const loggedInRequest = (payload) => ({
     type: LOGGED_IN_REQUEST,
-    payload
+    payload,
 });
 
-export const getUsersRequest = payload => ({
+export const getUsersRequest = (payload) => ({
     type: GET_USERS_REQUEST,
-    payload
+    payload,
 });
 
-export const getPostsRequest = payload => ({
+export const getPostsRequest = (payload) => ({
     type: GET_POSTS_REQUEST,
-    payload
+    payload,
+});
+
+export const deleteUserRequest = (payload) => ({
+    type: DELETE_USER_REQUEST,
+    payload,
 });
 
 export const logoutRequest = () => ({
-    type: LOGOUT_REQUEST
+    type: LOGOUT_REQUEST,
 });
