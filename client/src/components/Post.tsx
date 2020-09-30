ab4fe04db574d4b56aac11ae5828b58c696e20d4
Andrés Saam <andressaa.dev@gmail.com>
2020-09-30 15:31:23 -0500

    Instalando y usando moment.js para manejar las fechas de creación y actualización de las publicaciones

--- client/src/components/Post.tsx
+++ client/src/components/Post.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { Box, Container, Typography, CardMedia } from "@material-ui/core";
+import moment from "moment";
+import "moment/locale/es-mx";
 
 function Post({ post }: { post?: any }) {
+    moment.locale("es-mx");
+    let creationDateSp = moment(post?.created_at).fromNow();
+    let updatedDateSp = moment(post?.updated_at).fromNow();
+
     return (
         <>
             <Box my={4}>
                 <Container maxWidth="md">
-                    <Typography variant="h2" align="center">
+                    <Typography variant="h2" align="center" gutterBottom={true}>
                         {post?.title}
+                    </Typography>
+                    <Typography variant="subtitle1" gutterBottom={true}>
+                        Creado: {creationDateSp} | Actualizado: {updatedDateSp}
                     </Typography>
                     <Typography
                         variant="h6"
